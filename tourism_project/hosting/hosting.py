e186970b5215afebae1bc5810db9c473d8626cdb
VamsiKrishnaBakkamunthala <76095598+vamsikrishna-1530@users.noreply.github.com>
2025-10-05 00:10:24 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -3,8 +3,8 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="week_3_mls/deployment",     # the local folder containing your files
-    repo_id="praneeth232/Machine-Failure-Prediction",          # the target repo
+    folder_path="tourism_project/deployment",     # the local folder containing your files
+    repo_id="vamsikrishna1516/Tourism_Prediction_App",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
